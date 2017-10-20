23d42c6415ba3e61a65f776d80e52186b440b6a3
alokaku04 <alok.ranjan@dal.ca>
2017-10-20 16:25:33 -0300

    final

--- task3-b.py
+++ task3-b.py
@@ -1,14 +1,15 @@
 f=open('running-config.cfg','r')
-x=[]
+
 for line in f:
     if "access" in line:
         line=line.strip()
-        x.append(tuple(line.split("object")))
-print(x)
-d={}
-for item in x:
-    i=0
-    while i<len(item):
 
-            d[item[0]]=item[1]
+
+        x=(tuple(line.split("object")))
+d={}
+i=0
+while i<len(x):
+    if "access" in x[i]:
+        d[x[i]]=x[i+1]
+        print(d)
     i+=1
