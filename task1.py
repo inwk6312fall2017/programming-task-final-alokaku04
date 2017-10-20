db885a61945e38f485b0cd3d90664dd81131bd56
alokaku04 <alok.ranjan@dal.ca>
2017-10-20 13:48:10 -0300

    word in list

---
+++ task1.py
@@ -0,0 +1,11 @@
+book1=open("Book1.txt","r")
+y1=book1.read()
+x1=y1.split()
+
+book2=open("Book2.txt","r")
+y2=book1.read()
+x2=y2.split()
+
+book3=open("Book3.txt","r")
+y3=book3.read()
+x3=y3.split()
